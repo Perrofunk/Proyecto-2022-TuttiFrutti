4e850c7f9627efad3e527eb2c33e660e41fc3341
Rodrigo <gatopunk99@gmail.com>
2022-07-15 03:33:05 -0300

    Factory Seeders
    Sistemas Digitales

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $name=$this->faker->unique()->sentence();
+        return [
+            'name'=>$name,
+            'slug'=>Str::slug($name),
+            'extract'=>$this->faker->text(250),
+            'body'=>$this->faker->text(2000),
+            'status'=>$this->faker->randomElement([1, 2]),
+            'category_id'=>Category::all()->random()->id,
+            'user_id'=>User::all()->random()->id
+            
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +19,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
+        Storage::makeDirectory('public/posts');
+        $this->call(UserSeeder::class);
+        Category::factory(4)->create();
+        Tag::factory(8)->create();
+        $this->call(PostSeeder::class);
+        
     }
 }
